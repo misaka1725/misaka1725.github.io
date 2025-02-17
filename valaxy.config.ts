b25b56dd257dd1587d0f3c6e294bca4aafaffd08
bGlzdGRlcg <listder@outlook.com>
2025-02-17 21:56:49 +0800

    rm useless things

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,16 +1,11 @@
 import type { UserThemeConfig } from 'valaxy-theme-yun'
 import { defineValaxyConfig } from 'valaxy'
 
-// add icons what you will need
 const safelist = [
   'i-ri-home-line',
 ]
 
-/**
- * User Config
- */
 export default defineValaxyConfig<UserThemeConfig>({
-  // site config see site.config.ts
   theme: 'valaxy-theme-yun',
   themeConfig: {
     banner: {
@@ -24,25 +19,9 @@ export default defineValaxyConfig<UserThemeConfig>({
       opacity: 1
     },
     pages: [
-      // {
-      //   name: '我的小伙伴们',
-      //   url: '/links/',
-      //   icon: 'i-ri-genderless-line',
-      //   color: 'dodgerblue',
-      // },
-      // {
-      //   name: '喜欢的小可爱们',
-      //   url: '/girls/',
-      //   icon: 'i-ri-women-line',
-      //   color: 'hotpink',
-      // },
     ],
     footer: {
       since: 2024,
-      //beian: {
-      //  enable: false,
-      //  icp: '苏ICP备17038157号',
-      //},
     },
   },
   unocss: { safelist },
